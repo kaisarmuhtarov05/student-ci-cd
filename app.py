233c521ca05d484cd84bd7a4d4fa277a016e09a1
Kaisar <kaisarmuhtarov05@gmail.com>
2026-04-23 10:26:33 +0500

    Initial commit (improved CI/CD)

---
+++ app.py
@@ -0,0 +1,7 @@
+﻿def add(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise ValueError("Тек сан болуы қажет")
+    return a + b
+
+if __name__ == "__main__":
+    print("Нәтиже:", add(2, 3))
---
+++ test_app.py
@@ -0,0 +1,15 @@
+﻿import pytest
+from app import add
+
+def test_add_positive():
+    assert add(2, 3) == 5
+
+def test_add_negative():
+    assert add(-1, -1) == -2
+
+def test_add_float():
+    assert add(2.5, 2.5) == 5.0
+
+def test_add_invalid():
+    with pytest.raises(ValueError):
+        add("2", 3)
