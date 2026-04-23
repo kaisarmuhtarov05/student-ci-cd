5825e830066301b6712246da03d62d196419aa38
Kaisar <kaisarmuhtarov05@gmail.com>
2026-04-23 11:52:07 +0500

    Initial commit (improved CI/CD)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-﻿def add(a, b):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise ValueError("Тек сан болуы қажет")
-    return a + b
+﻿
+import math
+
+a = 49
+def tubir(a):    
+    return math.sqrt(a)
 
 if __name__ == "__main__":
-    print("Нәтиже:", add(2, 3))
+    print("Нәтиже:", tubir(a))
